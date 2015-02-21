56e9e5ce0f63e8426f35a69fd8a06d6fa762fc47
Jasper Abbink <git@abb.ink>
2015-02-21 20:15:45 +0100

    Fix to actually generate the correct sequence

--- src/ink/abb/rxchallenges/RxConway.scala
+++ src/ink/abb/rxchallenges/RxConway.scala
@@ -1,47 +1,70 @@
 package ink.abb.rxchallenges
 
-import rx.lang.scala.{Observable}
+import rx.lang.scala.Observable
 
+/*
+ * Naive parser of an Observable[Int] to create the next sequence (to be used with flatMap)
+ * Is cold observable for flatMap and needs to reset its internal state when onCompleted is triggered
+ * otherwise the returned sequence will be wrong
+*/
 class RxConwayParser {
   var previous = -1
   var sameCount = 0
 
-  def onNext(value: Int): Observable[Int] = {
+  def onNext(value: Int) = {
+    // variable to store result
     var result: Observable[Int] = null
     if (previous == -1 || previous == value) {
       sameCount = sameCount + 1
+      // we still got the same number, so return nothing at all
       result = Observable.empty
     } else {
+      // new number, return an observable with two numbers from the next sequence
       result = Observable.just(sameCount, previous)
       sameCount = 1
     }
     previous = value
-    return result
+    result
   }
 
-  def onCompleted(): Observable[Int] = {
-    return Observable.just(sameCount, previous)
+  def onCompleted() = {
+    val result = Observable.just(sameCount, previous)
+    reset()
+    result
   }
 
-  def onError(e: Throwable): Observable[Int] = {
-    return Observable.empty
+  def reset() = {
+    previous = -1
+    sameCount = 0
+  }
+
+  def onError(e: Throwable) = {
+    Observable.empty
   }
 }
 
 class RxConway(start: Observable[Int]) {
   def this() = this(Observable.just(1))
 
-  def next: RxConway = {
+  def next = {
     val parser = new RxConwayParser
     val result = start.flatMap(parser.onNext, parser.onError, parser.onCompleted)
-    return new RxConway(result)
+    new RxConway(result)
   }
 
-  start.subscribe(value => print(s"$value "), e => None, () => println())
+  def subscribe(onNext: Int => Unit, onError: Throwable => Unit, onCompleted: () => Unit) = {
+    start.subscribe(onNext, onError, onCompleted)
+  }
 }
 
 object RxConway {
   def main(args: Array[String]): Unit = {
-    (new RxConway).next.next.next.next.next.next.next.next.next.next.next.next
+    val repetitions = Observable.just(0).repeat(20)
+
+    var conwaySequence = new RxConway
+    repetitions.subscribe(_ => {
+      conwaySequence.subscribe(value => print(value), e => None, () => println())
+      conwaySequence = conwaySequence.next
+    })
   }
 }
